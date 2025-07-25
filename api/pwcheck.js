8acee0f399d3fa3f0fd983222187c0bda62d8970
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-07-25 11:07:26 +0200

    add smth to pwcheck

--- api/pwcheck.js
+++ api/pwcheck.js
@@ -1,9 +1,12 @@
 export default function handler(req, res) {
   const { pw } = req.query;
 
+  // TADY MĚŇ HESLO, NE JINDE
+  const heslo = "frconzole24";
+
   res.setHeader("Content-Type", "text/plain");
 
-  if (pw === "frconzole24") {
+  if (pw === heslo) {
     res.status(200).send("true");
   } else {
     res.status(200).send("false");
